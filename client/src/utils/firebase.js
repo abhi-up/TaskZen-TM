8058253cf8b379b7e7d6472be10cb300e82aa4ad
Abhi <meabhishek1512@gmail.com>
2024-04-11 14:40:46 +0530

    frontend: firebase setup

---
+++ client/src/utils/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
+    authDomain: "task-manager-abhi.firebaseapp.com",
+    projectId: "task-manager-abhi",
+    storageBucket: "task-manager-abhi.appspot.com",
+    messagingSenderId: "635912672982",
+    appId: "1:635912672982:web:bd48330d4b1b74a09d1b77",
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
