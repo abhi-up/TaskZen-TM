ae5c0936571ab4e1364251e33e45b6684a366155
Abhishek Upadhyay <77201101+abhi-up@users.noreply.github.com>
2024-04-13 01:47:52 +0530

    Updated cors

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const app = express()
 
 app.use(
     cors({
-        origin: ["http://localhost:3000", "http://localhost:3001", "https://task-manager-abhi.netlify.app/"],
+        origin: ["http://localhost:3000", "http://localhost:3001", "https://task-manager-abhi.netlify.app"],
         methods: ["GET", "POST", "DELETE", "PUT"],
         credentials: true,
     })
