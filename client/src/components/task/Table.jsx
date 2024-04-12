daca95d245128567115a67570332d5b857a4b030
Abhi <meabhishek1512@gmail.com>
2024-04-12 23:10:35 +0530

    list view: edit and delete task

--- client/src/components/task/Table.jsx
+++ client/src/components/task/Table.jsx
@@ -9,10 +9,12 @@ import {
 import { toast } from "sonner"
 import { BGS, PRIOTITYSTYELS, TASK_TYPE, formatDate } from "../../utils"
 import clsx from "clsx"
-import { FaList } from "react-icons/fa"
+import { FaList, FaWindowRestore } from "react-icons/fa"
 import UserInfo from "../UserInfo"
 import Button from "../Button"
 import ConfirmatioDialog from "../Dialogs"
+import { useTrashTaskMutation } from "../../redux/slices/api/taskApiSlice"
+import AddTask from "./AddTask"
 
 const ICONS = {
     high: <MdKeyboardDoubleArrowUp />,
@@ -23,13 +25,37 @@ const ICONS = {
 const Table = ({ tasks }) => {
     const [openDialog, setOpenDialog] = useState(false)
     const [selected, setSelected] = useState(null)
+    const [openEdit, setOpenEdit] = useState(false)
+
+    const [trashTask] = useTrashTaskMutation()
 
     const deleteClicks = (id) => {
         setSelected(id)
         setOpenDialog(true)
     }
 
-    const deleteHandler = () => {}
+    const editTaskHandler = (el) => {
+        setSelected(el)
+        setOpenEdit(true)
+    }
+
+    const deleteHandler = async () => {
+        try {
+            const result = await trashTask({
+                id: selected,
+                isTrash: "trash",
+            }).unwrap()
+            toast.success(result?.message)
+
+            setTimeout(() => {
+                setOpenDialog(false)
+                window.location.reload()
+            }, 500)
+        } catch (err) {
+            console.log(err)
+            toast.error(err?.data?.message || err.message)
+        }
+    }
 
     const TableHeader = () => (
         <thead className="w-full border-b border-gray-300">
@@ -119,6 +145,7 @@ const Table = ({ tasks }) => {
                     className="text-blue-600 hover:text-blue-500 sm:px-0 text-sm md:text-base"
                     label="Edit"
                     type="button"
+                    onClick={() => editTaskHandler(task)}
                 />
 
                 <Button
@@ -150,6 +177,13 @@ const Table = ({ tasks }) => {
                 open={openDialog}
                 setOpen={setOpenDialog}
                 onClick={deleteHandler}
+            />
+
+            <AddTask
+                open={openEdit}
+                setOpen={setOpenEdit}
+                task={selected}
+                key={new Date().getTime()}
             />
         </>
     )
