46789e2714865406d8b3afefb510bd69d7c4884d
Abhi <meabhishek1512@gmail.com>
2024-04-12 12:54:59 +0530

    warning fixed

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -214,7 +214,7 @@ const Dashboard = () => {
         )
     }
     return (
-        <div classNamee="h-full py-4">
+        <div className="h-full py-4">
             <div className="grid grid-cols-1 md:grid-cols-4 gap-5">
                 {stats.map(({ icon, bg, label, total }, index) => (
                     <Card
