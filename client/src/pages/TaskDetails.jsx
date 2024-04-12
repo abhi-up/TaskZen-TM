53b078d7a216b70c95902acc57dca0ff441fe808
Abhi <meabhishek1512@gmail.com>
2024-04-13 00:09:15 +0530

    task details: get single task & task activities functionality added

--- client/src/pages/TaskDetails.jsx
+++ client/src/pages/TaskDetails.jsx
@@ -19,6 +19,10 @@ import Tabs from "../components/Tabs"
 import { PRIOTITYSTYELS, TASK_TYPE, getInitials } from "../utils"
 import Loading from "../components/Loader"
 import Button from "../components/Button"
+import {
+    useGetSingleTaskQuery,
+    usePostTaskActivityMutation,
+} from "../redux/slices/api/taskApiSlice"
 
 const assets = [
     "https://images.pexels.com/photos/2418664/pexels-photo-2418664.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
@@ -88,9 +92,17 @@ const act_types = [
 
 const TaskDetails = () => {
     const { id } = useParams()
+    const { data, isLoading, refetch } = useGetSingleTaskQuery(id)
 
     const [selected, setSelected] = useState(0)
-    const task = tasks[3]
+    const task = data?.task
+
+    if (isLoading)
+        return (
+            <div className="py-10">
+                <Loading />
+            </div>
+        )
 
     return (
         <div className="w-full flex flex-col gap-3 mb-4 overflow-y-hidden">
@@ -249,7 +261,11 @@ const TaskDetails = () => {
                     </>
                 ) : (
                     <>
-                        <Activities activity={task?.activities} id={id} />
+                        <Activities
+                            activity={data?.task?.activities}
+                            id={id}
+                            refetch={refetch}
+                        />
                     </>
                 )}
             </Tabs>
@@ -257,12 +273,31 @@ const TaskDetails = () => {
     )
 }
 
-const Activities = ({ activity, id }) => {
+const Activities = ({ activity, id, refetch }) => {
     const [selected, setSelected] = useState(act_types[0])
     const [text, setText] = useState("")
-    const isLoading = false
 
-    const handleSubmit = async () => {}
+    const [postActivity, { isLoading }] = usePostTaskActivityMutation()
+    const handleSubmit = async () => {
+        try {
+            const activityData = {
+                type: selected?.toLowerCase(),
+                activity: text,
+            }
+
+            const result = await postActivity({
+                data: activityData,
+                id,
+            }).unwrap()
+
+            setText("")
+            toast.success(result?.message)
+            refetch()
+        } catch (err) {
+            console.log(err)
+            toast.error(err?.data?.message || err.message)
+        }
+    }
 
     const Card = ({ item }) => {
         return (
@@ -302,7 +337,7 @@ const Activities = ({ activity, id }) => {
                         <Card
                             key={index}
                             item={el}
-                            isConnected={index < activity.length - 1}
+                            isConnected={index < activity?.length - 1}
                         />
                     ))}
                 </div>
--- client/src/redux/slices/api/taskApiSlice.js
+++ client/src/redux/slices/api/taskApiSlice.js
@@ -63,6 +63,23 @@ export const taskApiSlice = apiSlice.injectEndpoints({
                 credentials: "include",
             }),
         }),
+
+        getSingleTask: builder.query({
+            query: (id) => ({
+                url: `${TASKS_URL}/${id}`,
+                method: "GET",
+                credentials: "include",
+            }),
+        }),
+
+        postTaskActivity: builder.mutation({
+            query: ({ data, id }) => ({
+                url: `${TASKS_URL}/activity/${id}`,
+                method: "POST",
+                body: data,
+                credentials: "include",
+            }),
+        }),
     }),
 })
 
@@ -74,4 +91,6 @@ export const {
     useUpdateTaskMutation,
     useTrashTaskMutation,
     useCreateSubTaskMutation,
+    useGetSingleTaskQuery,
+    usePostTaskActivityMutation,
 } = taskApiSlice
